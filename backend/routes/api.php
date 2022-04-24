228c2a300091090a6181f339106c6a780172addf
Thanhnguyen662 <thanhnguyen.devplus@gmail.com>
2022-04-24 17:40:25 +0700

    feat: 🎸 hard code for send mail

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\EmailController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -32,6 +33,16 @@ Route::prefix('/v1/auth')->group(
                         Route::get('/', 'getCurrentUser');
                     }
                 );
+            }
+        );
+    }
+);
+
+Route::prefix('/v1/mail')->group(
+    function () {
+        Route::controller(EmailController::class)->group(
+            function () {
+                Route::get('/test', 'index');
             }
         );
     }
