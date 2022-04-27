add215e332a337ce387d61af9cecddd9c1c6d4d1
Vu Quoc Nguyen <vuquocnguyen.devplus@gmail.com>
2022-04-27 09:45:08 +0700

    fix: 🐛 description of campaign detail page

--- frontend/src/features/Home/components/CampaignDetail/index.jsx
+++ frontend/src/features/Home/components/CampaignDetail/index.jsx
@@ -128,72 +128,7 @@ function CampaignDetail(props) {
                </div>
             </Col>
          </Row>
-         <Space direction='vertical' size={'middle'}>
-            <div>
-               <Title level={4}>Top 3 Reasons To Join Us</Title>
-               <ul style={{ listStyle: 'circle', paddingLeft: '30px' }}>
-                  <li>Chuyên gia đầu ngành </li>
-                  <li>Cơ hội phát triển nhanh </li>
-                  <li>Môi trường năng động sáng tạo </li>
-               </ul>
-            </div>
-            <div>
-               <Title level={4}>Description</Title>
-               <Text style={{ textAlign: 'justify' }}>
-                  {campaignDescription}
-               </Text>
-            </div>
-            <div>
-               <Title level={4}>Why You'll Love Working Here</Title>
-               <Text style={{ textAlign: 'justify' }}>
-                  <Title level={5}># Dẫn đầu thị trường</Title>
-                  <ul style={{ listStyle: 'circle', paddingLeft: '30px' }}>
-                     <li>
-                        Cơ hội được làm việc tại Tập đoàn tiên phong mở đường ,
-                        kiến tạo tương lai thông minh, gắn kết của Việt Nam.
-                     </li>
-                     <li>
-                        Cơ hội được thử sức trong những lĩnh vực công nghệ tiên
-                        tiến, hiện đại nhất.
-                     </li>
-                  </ul>
-                  <Title level={5}># Môi trường năng động sáng tạo</Title>
-                  <ul style={{ listStyle: 'circle', paddingLeft: '30px' }}>
-                     <li>
-                        Môi trường làm việc cởi mở và năng động, khuyến khích
-                        trao đổi ý tưởng ở mọi cấp, cho phép bạn làm việc, sáng
-                        tạo theo cách riêng.
-                     </li>
-                     <li>
-                        Được khơi gợi cảm hứng làm việc với văn phòng xanh,
-                        không gian mở, hiện đại tiêu chuẩn quốc tế.
-                     </li>
-                  </ul>
-                  <Title level={5}># Cơ hội thử thách và phát triển</Title>
-                  <ul style={{ listStyle: 'circle', paddingLeft: '30px' }}>
-                     <li>
-                        Áp dụng kiến thức, kĩ năng, kinh nghiệm chuyên môn giải
-                        quyết các bài toán hấp dẫn, thử thách tại Viettel.
-                     </li>
-                     <li>
-                        Cơ hội thử sức ở nhiều lĩnh vực, sản phẩm và quốc gia
-                        khác nhau góp phần thay đổi xã hội .
-                     </li>
-                     <li>
-                        Viettel cam kết cho bạn nền tảng vững chắc để học hỏi và
-                        phát triển.
-                     </li>
-                  </ul>
-                  <Title level={5}># Chế ngộ đãi ngộ hấp dẫn, cạnh tranh</Title>
-                  <ul style={{ listStyle: 'circle', paddingLeft: '30px' }}>
-                     <li>
-                        Lương thưởng cạnh tranh trên thị trường, phản ánh đúng
-                        năng lực thực tế.
-                     </li>
-                  </ul>
-               </Text>
-            </div>
-         </Space>
+         <Text>{campaignDescription} </Text>
       </CampaignDetailWrapper>
    );
 }
