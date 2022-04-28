94e53a5cab496b9454156c32804e0cb9641f6e0f
huuchinh <chinhthaihuu.devplus@gmail.com>
2022-04-28 19:38:51 +0700

    feat: re-migration table

--- backend/database/migrations/2022_04_28_062347_update_profile_table.php
+++ backend/database/migrations/2022_04_28_062347_update_profile_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('profiles', function (Blueprint $table) {
-            $table->timestamps();
-        });
+        // Schema::table('profiles', function (Blueprint $table) {
+        //     $table->timestamps();
+        // });
     }
     /**
      * Reverse the migrations.
