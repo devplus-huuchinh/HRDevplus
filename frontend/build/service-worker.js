fafd8d59c2607a84e728d69a7b451da88c3c38f1
huuchinh <chinhthaihuu.devplus@gmail.com>
2022-05-13 10:37:34 +0700

    deploy new

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.71e0bc80b19232341a86be4f03a43014.js"
+  "/precache-manifest.8b9d5fcf63990b39ddc97e8a19894e3e.js"
 );
 
 self.addEventListener('message', (event) => {
